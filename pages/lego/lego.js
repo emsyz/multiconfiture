8dc78162c26820ad79fead3faee75afdcbf20b28
Julien Salvatore <julien.salvatore@gmail.com>
2024-02-10 15:26:32 +0100

    imported girafe lego

--- pages/lego/lego.js
+++ pages/lego/lego.js
@@ -1,4 +1,10 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
+/**
+ * Base
+ */
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -7,25 +13,85 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 /**
- * Object
+ * Models
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff00ff });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+// Imported models
+const gltfLoader = new GLTFLoader();
 
-// Sizes
+gltfLoader.load("/3D/mesh/lego_resolved/head.gltf", (gltf) => {
+  const lego = gltf.scene.children[0];
+  lego.receiveShadow = true;
+  lego.castShadow = true;
+  scene.add(lego);
+});
+
+gltfLoader.load("/3D/mesh/lego_resolved/neck.gltf", (gltf) => {
+  const lego = gltf.scene.children[0];
+  lego.receiveShadow = true;
+  lego.castShadow = true;
+  scene.add(lego);
+});
+
+gltfLoader.load("/3D/mesh/lego_resolved/body.gltf", (gltf) => {
+  const lego = gltf.scene.children[0];
+  lego.receiveShadow = true;
+  lego.castShadow = true;
+  scene.add(lego);
+});
+
+gltfLoader.load("/3D/mesh/lego_resolved/backleg.gltf", (gltf) => {
+  const lego = gltf.scene.children[0];
+  lego.receiveShadow = true;
+  lego.castShadow = true;
+  scene.add(lego);
+});
+
+gltfLoader.load("/3D/mesh/lego_resolved/frontleg.gltf", (gltf) => {
+  const lego = gltf.scene.children[0];
+  lego.receiveShadow = true;
+  lego.castShadow = true;
+  scene.add(lego);
+});
+
+/**
+ * Floor
+ */
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(10, 10),
+  new THREE.MeshStandardMaterial({
+    color: "#444444",
+    metalness: 0,
+    roughness: 0.5,
+  })
+);
+floor.receiveShadow = true;
+floor.rotation.x = -Math.PI * 0.5;
+scene.add(floor);
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 2.4);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1.8);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.set(1024, 1024);
+directionalLight.shadow.camera.far = 15;
+directionalLight.shadow.camera.left = -7;
+directionalLight.shadow.camera.top = 7;
+directionalLight.shadow.camera.right = 7;
+directionalLight.shadow.camera.bottom = -7;
+directionalLight.position.set(5, 5, 5);
+scene.add(directionalLight);
+
+/**
+ * Sizes
+ */
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
-
-/**
- * Camera
- */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
-scene.add(camera);
 
 window.addEventListener("resize", () => {
   // Update sizes
@@ -41,19 +107,48 @@ window.addEventListener("resize", () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
-const clock = new THREE.Clock();
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.set(2, 2, 2);
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.target.set(0, 0.75, 0);
+controls.enableDamping = true;
+
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-renderer.render(scene, camera);
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+let previousTime = 0;
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - previousTime;
+  previousTime = elapsedTime;
+
+  // Update controls
+  controls.update();
 
   // Render
   renderer.render(scene, camera);
